ccb79daf0c448c3e04b2293c8895c309bb5922c8
blacke4gle <ahmetdemirr0@gmail.com>
2023-03-28 01:46:52 +0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43a61808-f145-4035-9cc8-30681abe47af';
+export const MODAL_CLOSE_SEC = 2.5;
